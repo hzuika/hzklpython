ff6b3dd773fa652eb08346854d4a6175ed4a9593
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-07-25 22:05:47 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import hzkl
+
+setup(name='hzkl',
+      author='Yuki Hashimoto',
+      author_email='',
+      maintainer='Yuki Hashimoto',
+      maintainer_email='',
+      description='',
+      long_description='',
+      license='MIT',
+      url='https://github.com/hzuika/hzklpython',
+      version=hzkl.__version__,
+      download_url='https://github.com/hzuika/hzklpython',
+      python_requires='>=3.6',
+      install_requires=[],
+      extras_require=[],
+      packages=['hzkl'],
+      classifiers=[]
+    )
